44ca6723a255561ea17891fb83bcad982d35d7d9
shivanigu <shivanig.ug20.ece@nitp.ac.in>
2021-12-30 08:46:31 +0530

    after update

--- lib/landing.dart
+++ lib/landing.dart
@@ -1,54 +1,46 @@
-// ignore_for_file: sized_box_for_whitespace
-
+// import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_signin_button/flutter_signin_button.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Begining(),
+    home: Landing_Page(),
   ));
 }
 
-class Begining extends StatelessWidget {
-  const Begining({Key? key}) : super(key: key);
+class Landing_Page extends StatelessWidget {
+  const Landing_Page({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     double height = MediaQuery.of(context).size.height;
-    double width = MediaQuery.of(context).size.width;
 
     return Scaffold(
-      // resizeToAvoidBottomPadding: false,
       body: Column(
         children: [
           Stack(
             children: [
               Image.asset(
                 'assests/image 1.png',
-                // height: 600,
                 height: height / 1.35,
-                // width: width,
                 width: 500,
                 fit: BoxFit.values[0],
               ),
               Positioned(
                   bottom: 30,
-                  // left: 70,
                   child: Padding(
                     padding: const EdgeInsets.fromLTRB(60.0, 0, 60.0, 0),
                     child: Container(
                       width: 273,
                       height: 60.0,
-                      child: const Text(
+                      child: Text(
                         'A new way to share\nyour journey',
                         style: TextStyle(
                             fontSize: 25.0,
-                            fontWeight: FontWeight.bold,
-                            fontFamily: 'Roboto'
-                            // fontStyle: FontStyle.
-                            ),
-                        // word
+                            // fontWeight: FontWeight.bold,
+                            fontWeight: FontWeight.w700,
+                            fontFamily: 'Roboto'),
                       ),
                     ),
                   ))
@@ -57,28 +49,25 @@ class Begining extends StatelessWidget {
           FractionallySizedBox(
             widthFactor: 0.8,
             child: FlatButton(
+              onPressed: () {},
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(6.0),
               ),
-              onPressed: () {},
               child: Text(
                 'Continue with Phone Number',
                 style: TextStyle(fontFamily: 'Roboto'),
               ),
-              color: Colors.blueAccent,
+              color: Color(0xFF478DF4),
               textColor: Colors.white,
             ),
           ),
-          // SizedBox(height: 6.0),
           Text(
-            'or',
+            'Or',
             style: TextStyle(
               fontSize: 22.0,
-              color: Colors.grey,
-              // color:
+              color: Color.fromRGBO(122, 73, 73, 0.72),
             ),
           ),
-          // // RaisedButton.icon(onPressed: () {}, icon: Icons., label: )
           FractionallySizedBox(
             widthFactor: 0.8,
             child: SignInButton(
@@ -92,14 +81,10 @@ class Begining extends StatelessWidget {
           Padding(
             padding: const EdgeInsets.all(10.0),
             child: Text(
-                'By continuing, you agree to our terms and conditions and privacy policy'),
-          )
-// with custom text
-          // SignInButton(
-          //   Buttons.Google,
-          //   text: "Sign up with Google",
-          //   onPressed: () {},
-          // )
+              'By continuing, you agree to our terms and conditions and privacy policy',
+              style: TextStyle(color: Color.fromRGBO(122, 73, 73, 0.72)),
+            ),
+          ),
         ],
       ),
     );
